b744e6a7e94e79a6d598f7b7810464156a4eeb0a
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-06-10 16:25:51 +0530

    patterns added

---
+++ Patterns/pattern1.java
@@ -0,0 +1,32 @@
+/*
+N=3
+  1
+ 232
+34543
+*/
+import java.util.Scanner;
+
+public class pattern1 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        print(N);
+    }
+	public static void print(int N) {
+        int c=0;
+		for(int i=1;i<=N;i++){
+            for(int sp=N-1;sp>=i;sp--)
+                System.out.print(" ");
+            for(int j=1;j<=i;j++){
+                c++;
+                System.out.print(c);
+            }
+            for(int k=i-1;k>0;k--){
+                c--;
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+	}
+}
---
+++ Patterns/pattern2.java
@@ -0,0 +1,31 @@
+/*
+N=4
+   1
+  23
+ 345
+4567
+*/
+import java.util.Scanner;
+
+public class pattern2 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        print(N);
+    }
+	public static void print(int N) {
+        for(int i=1;i<=N;i++){
+            for(int sp=N-1;sp>=i;sp--){
+                System.out.print(" ");
+            }
+            int n=i;
+            for(int j=i;j>0;j--){
+                System.out.print(n);
+                n++;
+            }
+            System.out.println();
+        }
+    }
+
+}
