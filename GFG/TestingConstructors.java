b93f92f5222b72f22dd2a7dc4125e3e575a8d8bf
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-08-20 00:59:04 +0530

    testing constructors

--- GFG/TestingConstructors.java
+++ GFG/TestingConstructors.java
@@ -1,5 +1,22 @@
 package GFG;
 
+class A{
+	 protected int test;
+	A(){
+		System.out.println("A primary constructor");
+	}
+	A(int a){
+		System.out.println("A class");
+	}
+}
+class B extends A{
+	B(){
+		super(2);
+		System.out.println("B class");
+		A ob=new A();
+		ob.test=5;
+	}
+}
 public class TestingConstructors {
 	int a;
 	TestingConstructors ob;
@@ -8,8 +25,13 @@ public class TestingConstructors {
 	}
 
 	public static void main(String[] args) {
+//		TestingConstructors ob=new TestingConstructors();   //Error
 		TestingConstructors ob=new TestingConstructors(2);
+		B obj=new B();
 		System.out.println(ob.ob);
+		obj.test=5;
+		A obj1=new A();
+		obj.test=5;
 
 	}
 
